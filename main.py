572bcd26d2a2b3c2ac7b0b92aa9f02cb22560b9c
ycchen <egositinr.cs07g@nctu.edu.tw>
2022-08-06 12:32:40 +0000

    feat: Add several tasks in main script

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 Author: Egoist
 Date: 2022-06-10 15:43:23
 LastEditors: Egoist
-LastEditTime: 2022-07-18 09:52:01
+LastEditTime: 2022-08-06 02:44:06
 FilePath: /smp/main.py
 Description: 
     run different task in this script
 '''
 #%%
 import numpy as np
+import pandas as pd
 import torch
 import matplotlib.pyplot as plt
 
@@ -18,17 +19,30 @@ from train import main_cmdargv
 from detect import Detector
 import api
 
-def load_TMdataset():
-    return TMbaseset(datapath=['dataset/TMbase/data_200501_211031.csv',
-                               'dataset/TMbase/data_2111.csv',
-                               'dataset/TMbase/data_2112.csv',
-                               'dataset/TMbase/data_2201.csv',
-                               'dataset/TMbase/data_2202.csv',
-                               'dataset/TMbase/data_2203.csv',
-                               'dataset/TMbase/data_2204.csv',],)
+DATAPATH=['dataset/TMbase/data_200501_211031.csv',
+          'dataset/TMbase/data_2111.csv',
+          'dataset/TMbase/data_2112.csv',
+          'dataset/TMbase/data_2201.csv',
+          'dataset/TMbase/data_2202.csv',
+          'dataset/TMbase/data_2203.csv',
+          'dataset/TMbase/data_2204.csv',]
+
 
 def filter_userlist(floors=[4,11,15,18]):
     return TMbaseset.filter_use_list(TMbaseset.load_use_list('dataset/TMbase/use_list4.json'),floors=floors)
+
+def detect_user_period(expname,
+                       user,
+                       date=f'2020-5-1',
+                       days=730,
+                       threshold=0.5,
+                       showerr=True):
+    det=Detector(expname=expname,
+                 datasetpath=DATAPATH,
+                 device=torch.device('cpu'),
+                 tbwriter=None)
+    det.detect_day(model_NFA=user,data_NFA=user,date=date,days=days,metric='mape',threshold=threshold,showerr=showerr)
+    ...
 
 def detect_compute_ratio(expname,
                          date=f'2020-5-1',
@@ -36,14 +50,8 @@ def detect_compute_ratio(expname,
                          output_place=None,
                          thresholds=(0.5,0.6,0.7),
                          floors=[4,11,15,18]):
-    det=Detector(modelpath_prefix=f'exp/{expname}/model',
-                 datasetpath=['dataset/TMbase/data_200501_211031.csv',
-                              'dataset/TMbase/data_2111.csv',
-                              'dataset/TMbase/data_2112.csv',
-                              'dataset/TMbase/data_2201.csv',
-                              'dataset/TMbase/data_2202.csv',
-                              'dataset/TMbase/data_2203.csv',
-                              'dataset/TMbase/data_2204.csv',],
+    det=Detector(expname=expname,
+                 datasetpath=DATAPATH,
                  device=torch.device('cpu'),
                  tbwriter=output_place)
     userlist=filter_userlist(floors=floors)
@@ -63,14 +71,8 @@ def detect_apply_on_other_data(expname,
                                days=730,
                                output_place=None,
                                floors=[4,11,15,18]):
-    det=Detector(modelpath_prefix=f'exp/{expname}/model',
-                 datasetpath=['dataset/TMbase/data_200501_211031.csv',
-                              'dataset/TMbase/data_2111.csv',
-                              'dataset/TMbase/data_2112.csv',
-                              'dataset/TMbase/data_2201.csv',
-                              'dataset/TMbase/data_2202.csv',
-                              'dataset/TMbase/data_2203.csv',
-                              'dataset/TMbase/data_2204.csv',],
+    det=Detector(expname=expname,
+                 datasetpath=DATAPATH,
                  device=torch.device('cpu'),
                  tbwriter=output_place)
     userlist=filter_userlist(floors=floors)
@@ -91,6 +93,7 @@ def detect_apply_on_other_data(expname,
 def train_model():
     def make_argv(): #return different NFA's traing arguments
         device=1 #GPU index on training
+        torch.cuda.set_device(device)
         cond1=filter_userlist(floors=[4,11,15,18])
         return [["--dataset", "TMbase",
                  "--datapath","data_200501_211031.csv","data_2111.csv","data_2112.csv",
@@ -104,7 +107,7 @@ def train_model():
                  "--normalized_method","",
 
                  "--stack_types", "c",
-                 "--nb_blocks_per_stack", "2",
+                 "--nb_blocks_per_stack", "10",
                  "--backbone_layers", "2",
                  "--backbone_kernel_size", "3",
                  "--hidden_layer_units", "8",
@@ -114,18 +117,20 @@ def train_model():
                  "--context_size","6",
 
                  "--name",NFA,
-                 "--expname","ablation_basis_wo_constrain",
+                 "--expname","ablation_block10_a00_woc",
                  "--epochs", "200",
                  "--cudadevice", f"{device}",
                  "--rngseed", "6666",
                  "--trainlosstype","mape",
-                 "--lossratio","0,0,1,0.5",
+                 "--lossratio","0,0,1,0",
                  "--evaluatemetric","mape",
-                 "--train_batch","128",
+                 "--train_batch","32",
                  "--train_negative_batch","256",
+                 "--context_visualization","",
+                 "--samplesize","2",
                 ] for NFA in cond1[device::2]]
                 #  ] for NFA in ['N16-F04-A01']]
-
+        
     for s in make_argv():
         main_cmdargv(s)
     ...
@@ -143,20 +148,39 @@ def plot_disentangle_schematic():
     plt.show()
     ...
 
-def plot_user_data(user='N16-F04-A01',days=8,start_date=None):
-    load_TMdataset().plot_user(user,days,start_date)
+def plot_user_data(user='N16-F04-A01',days=8,start_date=None,figsize=(30,10)):
+    TMbaseset(datapath=DATAPATH).plot_user(user,days,start_date,figsize)
 
 def plot_model_basis(expname='B2_L2_K3_U8_T3_C6_align24_a05_ep200',
                      NFA='N16-F04-A01',
                      device=torch.device('cpu')):
-    model=NBeatsNet.build(f'exp/{expname}/model/{NFA}.mdl',new_name=NFA,new_device=device)
+    model=NBeatsNet.build(f'exp/{expname}/model/{NFA}.mdl',new_name=f'{expname}/{NFA}',new_device=device)
     fig=model.plot_basis()
     plt.show(block=False)
     ...
 
+def plot_model_result(expname,
+                      NFA,
+                      date=f'2020-5-1',
+                      days=1,
+                      device=torch.device('cpu')):
+    model=NBeatsNet.build(f'exp/{expname}/model/{NFA}.mdl',new_name=NFA,new_device=device)
+    set=TMbaseset(DATAPATH)
+    date=date if isinstance(date,pd.Timestamp) else pd.Timestamp(date)
+    target=set.getitembydate(date,length=days)[NFA].to_numpy(dtype=np.float32)
+    x=torch.from_numpy(target).unsqueeze(0)
+    inference=model.inference(x,future=None,step=1,trmode=False,gd=False)
+    fig, ax1 = plt.subplots()
+    ax1.plot(inference['backcast'].squeeze(),color='C1')
+    plt.show(block=False)
+
 def access_api_data(startdate=(2022,5,1),
                     enddate=(2022,5,1),
                     outdir='tempdata'):
+    '''need to open proxy server before run the code
+       1.ssh connect to server
+       2.run openproxyserver.bat
+       3.run this code'''
     api.main(startdate=startdate,
              enddate=enddate,
              outdir=outdir)
@@ -166,18 +190,41 @@ if __name__=='__main__':
     '''run the task
        adjust argument in above function if needed'''
 
-    # access_api_data(startdate=(2022,5,1),
-    #                 enddate=(2022,5,1))
+    exp=['ablation_block2',
+         'ablation_block2_a00'
+         'ablation_block2_a00_woc',
+         'ablation_block3',
+         'ablation_block3_a00'
+         'ablation_block3_a00_woc',
+         'ablation_block4',
+         'ablation_block4_a00'
+         'ablation_block4_a00_woc',]
+
+    access_api_data(startdate=(2022,7,1),
+                    enddate=(2022,7,31))
 
     # plot_disentangle_schematic()
+    
+    # [plot_user_data(user='N17-F04-A13',days=1,start_date=f'2021-5-{i}',figsize=None) for i in range(1,10)]
 
-    # plot_user_data()
+    # plot_model_basis(expname='ablation_block4_a00_woc',
+    #                  NFA='N16-F04-A09')#NFA='N19-F11-A01'
 
-    # plot_model_basis(expname='ablation_basis_wo_constrain',
-    #                  NFA='N16-F04-A09')
-
+    
     # train_model()
     
-    detect_compute_ratio(expname='ablation_basis_wo_constrain',output_place=None)
-    # detect_apply_on_other_data(expname='ablation_basis_wo_constrain',output_place='otherdata_ablation_basis_wo_constrain')
+    # detect_compute_ratio(expname='ablation_basis_wo_constrain',output_place=None)
+
+    # detect_apply_on_other_data(expname='ablation_block4bat32',output_place='otherdata_ablation_basis4')
+
+    # plot_model_result(expname='ablation_block4bat32_a00',
+    #                   NFA='N16-F04-A09',
+    #                   date=f'2021-5-1',
+    #                   days=1,)
+
+    # detect_user_period(expname='ablation_block2_a00_woc',
+    #                    user='N16-F04-A09',
+    #                    date=f'2021-5-1',
+    #                    days=7,
+    #                    threshold=0.5)
     ...
